aa2dee3b9a43ac049b48b3d3eca130211264da2e
Gleb <krishin@inbox.ru>
2019-10-15 11:51:20 +0300

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://Atom:y1567E5Iu27kU960@advancenode-shard-00-00-txlyp.mongodb.net:27017,advancenode-shard-00-01-txlyp.mongodb.net:27017,advancenode-shard-00-02-txlyp.mongodb.net:27017/test?ssl=true&replicaSet=AdvanceNode-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123'
+};
